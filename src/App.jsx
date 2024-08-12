da621143009563e7ad1725cd2634050250fb04f3
ImanolMoreno <Imanol_Moreno@sternenzelt.net.pe>
2024-08-12 11:54:10 -0500

    cambios antes de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Body from "./Components/Body/Body";
 import Footer from "./Components/Footer/Footer";
-import Community from './Components/Community/Community';
+// import Community from './Components/Community/Community';
 
 export default function App(){
 
   return(
     <div>
     <Header />
-        <Route path="/" element={<Body />} />
-        <Route path="/comunnity" element={<Community />} />
+        {/* <Route path="/" element={<Body />} />
+        <Route path="/comunnity" element={<Community />} /> */}
+    <Body/>
     <Footer />
   </div>
 );
