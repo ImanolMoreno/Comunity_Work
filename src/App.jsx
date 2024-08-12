dd3a6fb94eea8f6b3eaf7014212625371c083515
ImanolMoreno <Imanol_Moreno@sternenzelt.net.pe>
2024-08-12 17:41:29 -0500

    Add Modals to Front

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Header from "./components/Header/Header";
 import Body from "./Components/Body/Body";
 import Footer from "./Components/Footer/Footer";
 import Community from './Components/Community/Community';
+import RegisterModal from './Components/Modals/RegisterModal/RegisterModal';
+import LoginModal from './Components/Modals/LoginModal/LoginModal';
+
 
 export default function App() {
-  return (
 
-    //organizacio de rutas, agregar router englobando todo y routes para varias rutas
-    //agregar un nombre a las rutas y VERIFICAR EL NOMBRE CUAANDO SE LLAMA EN EL HEADER
+  const [isRegisterModalOpen, setRegisterModalOpen] = useState(false);
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+
+  const openRegisterModal = () => setRegisterModalOpen(true);
+  const closeRegisterModal = () => setRegisterModalOpen(false);
+
+  const openLoginModal = () => setLoginModalOpen(true);
+  const closeLoginModal = () => setLoginModalOpen(false);
+
+  return (
     <Router>
-      <Header />
+      <Header openRegisterModal={openRegisterModal} openLoginModal={openLoginModal} />
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/community" element={<Community />} name="community"/>
       </Routes>
+      <RegisterModal isRegisterModalOpen={isRegisterModalOpen} closeRegisterModal={closeRegisterModal}></RegisterModal>
+      <LoginModal isLoginModalOpen={isLoginModalOpen} closeLoginModal={closeLoginModal}></LoginModal>
       <Footer />
     </Router>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
 
-export default function Header() {
+
+export default function Header({openRegisterModal, openLoginModal}) {
   return (
     <header className="w-full bg-gray-800 text-white">
       <div className="flex justify-between items-center p-4">
@@ -41,119 +42,17 @@ export default function Header() {
           <button
             type="button"
             className="px-4 py-2 text-white rounded-md bg-blue-500"
-            onClick={() =>
-              document
-                .getElementById("registerModal")
-                .classList.remove("hidden")
-            }
+            onClick={openRegisterModal}
           >
             Registro
           </button>
           <button
             type="button"
             className="px-4 py-2 text-white rounded-md bg-red-600"
-            onClick={() =>
-              document.getElementById("loginModal").classList.remove("hidden")
-            }
+            onClick={openLoginModal}
           >
             Login
           </button>
-        </div>
-
-        {/* Modal de Registro */}
-        <div
-          id="registerModal"
-          className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 hidden"
-        >
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-            <span
-              className="text-gray-500 text-2xl cursor-pointer"
-              onClick={() =>
-                document.getElementById("registerModal").classList.add("hidden")
-              }
-            >
-              &times;
-            </span>
-            <h2 className="text-xl font-bold mb-4">Registro</h2>
-            <form id="registerForm" action="/register" method="POST">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Nombre"
-                required
-                className="mb-2 p-2 w-full border border-gray-300 rounded"
-              />
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Correo Electrónico"
-                required
-                className="mb-2 p-2 w-full border border-gray-300 rounded"
-              />
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Contraseña"
-                required
-                className="mb-4 p-2 w-full border border-gray-300 rounded"
-              />
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded w-full"
-              >
-                Registrar
-              </button>
-            </form>
-            <div id="register-success-message"></div>
-            <div id="register-error-message"></div>
-          </div>
-        </div>
-
-        {/* Modal de Login */}
-        <div
-          id="loginModal"
-          className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 hidden"
-        >
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-            <span
-              className="text-gray-500 text-2xl cursor-pointer"
-              onClick={() =>
-                document.getElementById("loginModal").classList.add("hidden")
-              }
-            >
-              &times;
-            </span>
-            <h2 className="text-xl font-bold mb-4">Login</h2>
-            <form id="loginForm" method="POST">
-              <input
-                type="email"
-                id="loginEmail"
-                name="email"
-                placeholder="Correo Electrónico"
-                required
-                className="mb-2 p-2 w-full border border-gray-300 rounded"
-              />
-              <input
-                type="password"
-                id="loginPassword"
-                name="password"
-                placeholder="Contraseña"
-                required
-                className="mb-4 p-2 w-full border border-gray-300 rounded"
-              />
-              <button
-                type="submit"
-                className="bg-red-600 text-white px-4 py-2 rounded w-full"
-              >
-                Ingresar
-              </button>
-            </form>
-            <div id="login-success-message"></div>
-            <div id="login-error-message"></div>
-          </div>
         </div>
       </div>
     </header>
